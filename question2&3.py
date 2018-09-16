9388298ea383a0185587c5009b82f44db62070c7
Ruwei Tang <rwtang1990@gmail.com>
2018-09-16 10:04:05 -0700

    Solution for Question2

--- question2&3.py
+++ question2&3.py
@@ -12,8 +12,20 @@ merchants_data = pd.read_csv(path + 'merchants.csv')
 comb_data = pd.merge(funnel_data, merchants_data, on = 'merchant_id', how = 'left')
 new_comb_data = pd.merge(loans_data, comb_data, on = 'merchant_id', how = 'left')
 
-#Question2
-
+#Question 2
+comb_data['date'] = comb_data['checkout_date']
+#Count the number of checkouts by day
+comb_data['num_loaded'] = countIf($G$2:$G$433327,H2).groupby('date')
+#Count the number of applied loans by day
+comb_data['num_loaded'] = COUNTIFS($G$2:$G$433327,H2,$D$2:$D$433327,"Loan Terms Run").groupby('date')
+#Count the number of approved loans by day
+comb_data['num_approved'] = COUNTIFS($G$2:$G$433327,H2,$D$2:$D$433327,"Loan Terms Approved").groupby('date')
+#Count the number of confirmed loans by day
+comb_data['num_confirmed'] = COUNTIFS($G$2:$G$433327,H2,$D$2:$D$433327,"Checkout Completed").groupby('date')
+# Calculate ratios
+comb_data['application_rate'] = num_applied / num_loaded
+comb_data['approval_rate'] = num_approved / num_applied
+comb_data['confirmation_rate'] = num_confirmed / num_approved
 
 #Question 3
 #Calculate revenue for each checkout
